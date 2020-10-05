805fde29104748085011a768d0d533449eaf0cda
Gennady Polakov <gennadypolakov@gmail.com>
2020-10-06 01:59:14 +0400

    webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,34 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: 'dist'
+          to: path.resolve(__dirname, 'dist')
         }
       ]
     }),
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
